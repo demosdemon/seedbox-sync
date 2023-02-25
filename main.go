5852831190bd1eb472f37e43716980ded79c705e
Brandon LeBlanc <brandon@leblanc.codes>
2023-02-25 17:27:18 +0000

    sort torrents by finished time

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sort"
 
 	"github.com/mrobinsn/go-rtorrent/rtorrent"
 )
@@ -20,6 +21,12 @@ func main() {
 	}
 
 	shared.log.INFO.Printf("Fetched %d torrents", len(torrents))
+
+	sort.Slice(torrents, func(i, j int) bool {
+		a := torrents[i].Finished
+		b := torrents[j].Finished
+		return a.Before(b)
+	})
 
 	for idx, torrent := range torrents {
 		name := fmt.Sprintf("Torrent %s", torrent.Name)
